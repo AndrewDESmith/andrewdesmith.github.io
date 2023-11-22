1960462fbc86a3f65c7b4c8bc3cf2cf958cfb4ec
Andrew Smith <andrew.d.e.smith@gmail.com>
2023-11-22 02:16:17 -0400

    Dynamically set caption text in modal.

--- js/modal.js
+++ js/modal.js
@@ -5,17 +5,17 @@ const modalClose = document.getElementById("modal-close");
 
 closeModal();
 
-imageSwitches.forEach((element) => {
-  openModal(element);
+imageSwitches.forEach((imageSwitch) => {
+  openModalFrom(imageSwitch);
 });
 
-function openModal(element) {
-  element.addEventListener("click", function () {
-    console.log("element", element);
+function openModalFrom(imageSwitch) {
+  imageSwitch.addEventListener("click", function () {
     modalBox.classList.remove("hidden");
     modalbg.classList.remove("hidden");
     let currentModalImage = document.getElementById("current-modal-image");
-    currentModalImage.src = element.src;
+    currentModalImage.src = imageSwitch.src;
+    setCaptionTextFrom(imageSwitch);
   });
 }
 
@@ -29,4 +29,39 @@ function closeModal() {
     modalBox.classList.add("hidden");
     modalbg.classList.add("hidden");
   });
+}
+
+function setCaptionTextFrom(imageSwitch) {
+  let captionText = document.getElementById("caption-text");
+  captionText.textContent = "New value.";
+
+  let imageFileName = imageSwitch.src.split("/screenshots/")[1];
+  imageFileName = imageFileName.replaceAll("%20", " ");
+
+  console.log("imageFileName", imageFileName);
+
+  switch (imageFileName) {
+    case "job-ad-builder/Job Ad Builder Search Snippets.png":
+      captionText.textContent = "Job Ad Builder shown...";
+      break;
+    case "facebook-jobs-api/Facebook Job Posting Censored.png":
+      captionText.textContent = "Facebook Job API...";
+      break;
+    case "better-file-review-status/Expanded Email Inbox File Review Statuses.png":
+      captionText.textContent = "Better file review status...";
+      break;
+    case "sentiment-analysis/Sentiment Analysis Censored.png":
+      captionText.textContent = "Sentiment analysis...";
+      break;
+    case "interactor-interface/Interactor Interface Code.png":
+      captionText.textContent = "Custom loggin interface...";
+      break;
+    case "job-apps-psychometric-tests/Psychometric and Environmental Fit Scales.png":
+      captionText.textContent = "Job applications psychometric revamp...";
+      break;
+    default:
+      console.log(
+        `The image file name of ${imageFileName} does not match the imageSwitch source value of ${imageSwitch.src}`
+      );
+  }
 }
