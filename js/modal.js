8699dc3cef80ed25ccf38b7be76c553f395b4c55
Andrew Smith <andrew.d.e.smith@gmail.com>
2023-11-24 03:24:00 -0400

    Finish writing and formatting captions.

--- js/modal.js
+++ js/modal.js
@@ -187,15 +187,15 @@ function setCaptionTextFrom(imageFileName) {
   switch (imageFileName) {
     case "job-ad-builder/Job Ad Builder Search Snippets.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[1 of 1]</span> &nbsp; Job Ad Builder shown...";
+        '<span class=\'font-gelasioBold text-ember-400\'>[1 of 1]</span> &nbsp; The job ad builder interface, showing a search of the "snippet" shared library of job ad fragments that the Hiring Success team could edit, delete, or add to; the latter from a separate page. Pressing the "insert" button on the lower right of the entry places the snippet into an in-app rich text editor found under the right tab, allowing rapid assembly and editing of a job ad. Snippets collections are organized under selectable industry categories and sub-categories ("positions"): e.g., Category: Engineering, Position: Civil Engineering. Further, each snippet is a general section of a job ad (e.g., "Culture Profile"). Here, we see all "Culture Profile" snippets with "floral" inside them, belonging to Customer Support & Service, Retail Customer Service positions.';
       break;
     case "facebook-jobs-api/Facebook Job Posting Censored.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[1 of 2]</span> &nbsp; Facebook Jobs API example posting.";
+        "<span class='font-gelasioBold text-ember-400'>[1 of 2]</span> &nbsp; A posting on the Facebook Jobs, supplied by daily syncronization with the Fitzii hiring software application.";
       break;
     case "facebook-jobs-api/Facebook Jobs Application Modal.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[2 of 2]</span> &nbsp; Facebook Jobs API modal...";
+        "<span class='font-gelasioBold text-ember-400'>[2 of 2]</span> &nbsp; A candidate hitting \"apply\" on Facebook Jobs sees this modal. Job application questions are supplied from the backend of the Fitzii hiring software application. Once the candidate submits their job application, a webhook in Fitzii receives and processes the submission.";
       break;
     case "better-file-review-status/Expanded Email Inbox File Review Statuses.png":
       captionText.innerHTML =
@@ -207,23 +207,23 @@ function setCaptionTextFrom(imageFileName) {
       break;
     case "sentiment-analysis/Sentiment Analysis Censored.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[1 of 1]</span> &nbsp; Sentiment analysis...";
+        "<span class='font-gelasioBold text-ember-400'>[1 of 1]</span> &nbsp; In-app view of automated sales campaign messages and prospect replies. Hightlighted in the center is the most recent reply, with auto-assigned sentiment. The right toolbar's highlight shows the current sentiment, which may be manually overridden by a user.";
       break;
     case "interactor-interface/Interactor Interface Code.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[1 of 1]</span> &nbsp; Custom loggin interface...";
+        '<span class=\'font-gelasioBold text-ember-400\'>[1 of 1]</span> &nbsp; Code for custom logging interface for large interactor objects, to assist in debugging. Developers that wish to log only important information such as a user\'s ID could pass the interactor object (the "context") and the desired ("whitelisted") attributes.';
       break;
     case "job-apps-psychometric-tests/Purpose and Meaning Psych Questions.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[1 of 3]</span> &nbsp; Job applications psychometric revamp...";
+        "<span class='font-gelasioBold text-ember-400'>[1 of 3]</span> &nbsp; The psychometric section of a job application. This section was enhanced from a previous version, designed to focus the applicant's attention more fully on a single question at a time, while showing them their progress through the variable length assessments.";
       break;
     case "job-apps-psychometric-tests/Psychometric and Environmental Fit Scales.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[2 of 3]</span> &nbsp; Job applications psychometric revamp...";
+        "<span class='font-gelasioBold text-ember-400'>[2 of 3]</span> &nbsp; The psychometric and environmental fit scales, following the job applicant's completion of the psychometric assessment. Employer-determined ranges for various areas are shown as gray horizontal shaded areas of each scale, with the applicant's score in each area shown as a black or red vertical bar. Overall applicant fit is shown in the mini-scale at the top left.";
       break;
     case "job-apps-psychometric-tests/Purpose and Meaning Psychometric Report.png":
       captionText.innerHTML =
-        "<span class='font-gelasioBold text-lightGray-50'>[3 of 3]</span> &nbsp; Job applications psychometric revamp...";
+        "<span class='font-gelasioBold text-ember-400'>[3 of 3]</span> &nbsp; Generated report summarizing six areas of traits, based on the job applicant's results in the posting's psychometric assessment.";
       break;
     default:
       console.log(
